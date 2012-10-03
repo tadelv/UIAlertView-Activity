fcbd288e3a5288c3372c78cd1ec40a5cfaafc676
Vid Tadel <tadelv@gmail.com>
2012-10-03 09:14:55 +0200

    Initial Commit

---
+++ AlertViewActivityDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AlertViewActivityDemo
+//
+//  Created by Vid Tadel on 10/3/12.
+//  Copyright (c) 2012 Research. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AlertViewActivityDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AlertViewActivityDemo
+//
+//  Created by Vid Tadel on 10/3/12.
+//  Copyright (c) 2012 Research. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
